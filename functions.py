6cf3e5dddbcfa452bbae8931d455e33a1a1e6795
chomosuke <38484873+chomosuke@users.noreply.github.com>
2019-04-22 12:54:53 +1200

    tablify with closest_factor added

--- functions.py
+++ functions.py
@@ -1,14 +1,24 @@
 import sys
 
-def progress_perc(i, len):
-	len1 = len // 100
-	if(i % (len1) == 0):
-		print(str(int(i / (len1))) + '0%', end=' ')
-		sys.stdout.flush()
+def progress_perc(i, len, width=100):
+    len_ = len // width
+    if len_ == 0 or i % len_ == 0:
+        print(str(round(i/len*100)) + '%', end=' ')
+        sys.stdout.flush()
 
-def progress_dots(i, len):
-	len1 = len // 100
-	if(i % (len1) == 0):
-		print('.', end='')
-		sys.stdout.flush()
+def progress_dots(i, len, width=100):
+    len_ = len // width
+    if len_ == 0 or i % len_ == 0:
+        print('.', end='')
+        sys.stdout.flush()
 
+def closest_factor(number, target):
+    last_factor = 1
+    for i in range(2, number + 1):
+        if number % i == 0: # is a factor
+            if i > target: # we have gone passed the target
+                if i - target <= target - last_factor: # i is closer
+                    return i;
+                else: # favor i as smaller percentage difference
+                    return last_factor;
+            last_factor = i
